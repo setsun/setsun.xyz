56d70f5a734bff0789b187a60548a55a28a64106
Setsun <setsun@setsun.io>
2018-01-06 04:00:42 -0500

    create CNAME record during deployment

--- bin/deploy-gh-pages.sh
+++ bin/deploy-gh-pages.sh
@@ -2,11 +2,12 @@
 
 echo "Deploying setsun.github.io"
 
-git branch -D deploy
-git branch -D staging
-
-git checkout -b staging
+git checkout -B staging
 rm .gitignore
+
+touch dist/CNAME
+echo 'setsun.io' >> dist/CNAME
+
 git add dist
 git -c user.name='CircleCI Deploy' -c user.email='foo@bar.com' commit -m "deploy" --no-verify
 
