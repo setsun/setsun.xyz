025110b90cd3a4aae287869514986caf40deacb0
Setsun <setsun@setsun.io>
2018-01-06 04:07:43 -0500

    blah don't polyfill

--- bin/deploy-gh-pages.sh
+++ bin/deploy-gh-pages.sh
@@ -9,9 +9,14 @@ touch dist/CNAME
 echo 'setsun.io' >> dist/CNAME
 
 git add dist
-git -c user.name='CircleCI Deploy' -c user.email='foo@bar.com' commit -m "deploy" --no-verify
+git -c user.name='CircleCI Deploy' -c user.email='deploy@setsun.io' commit -m "deploy" --no-verify
 
 git subtree split --prefix dist -b deploy
 git push -f origin deploy:master
+
+git reset --hard
+git co develop
+git branch -D staging
+git branch -D deploy
 
 echo "Deployed setsun.github.io"
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import 'aframe';
 import 'aframe-animation-component';
 import 'aframe-particle-system-component';
-import 'babel-polyfill';
+
 import { Entity, Scene } from 'aframe-react';
 import React from 'react';
 import ReactDOM from 'react-dom';
