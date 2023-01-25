437c6b0ba98aff84c021a60ef927a5d00531b19e
setsun <seanjeluo@gmail.com>
2023-01-24 19:29:42 -0500

    play around with responsive grid

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import React from "react"
 import { GetStaticProps } from "next"
+import * as Collapsible from "@radix-ui/react-collapsible";
 import Layout from "../components/Layout"
 import Post, { PostProps } from "../components/Post"
 import prisma from "../lib/prisma";
@@ -33,14 +34,21 @@ const Blog: React.FC<Props> = (props) => {
           <Post key={i} post={post} />
         ))}
 
-        <div className="grid grid-cols-3 gap-2 my-2">
-          <SpotifyIframePlaylist src="https://open.spotify.com/embed/playlist/1bUUPhe0zP9FgrsqYjxbTp?theme=0" />
-          <SpotifyIframePlaylist src="https://open.spotify.com/embed/playlist/1cUbQxIOFcxeL5oUheu85i?theme=0" />
-          <SpotifyIframePlaylist src="https://open.spotify.com/embed/playlist/3xx5COLUZ7Xwvyfg8MF4Nb?theme=0" />
-          <SpotifyIframePlaylist src="https://open.spotify.com/embed/playlist/4LddzZkIk08J3IwqXladlJ?theme=0" />
-          <SpotifyIframePlaylist src="https://open.spotify.com/embed/playlist/1aQK4Hz4Xmz3Y4NEhz9ReT?theme=0" />
-          <SpotifyIframePlaylist src="https://open.spotify.com/embed/playlist/26ItkYptyl56YEIYQDQs7r?theme=0" />
-        </div>
+        {/** todo: implement logic for open / close */}
+        <Collapsible.Root open>
+          <Collapsible.Trigger className="my-2">
+          </Collapsible.Trigger>
+
+          <Collapsible.Content className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2 my-2">
+            <SpotifyIframePlaylist src="https://open.spotify.com/embed/playlist/1bUUPhe0zP9FgrsqYjxbTp?theme=0" />
+            <SpotifyIframePlaylist src="https://open.spotify.com/embed/playlist/1cUbQxIOFcxeL5oUheu85i?theme=0" />
+            <SpotifyIframePlaylist src="https://open.spotify.com/embed/playlist/3xx5COLUZ7Xwvyfg8MF4Nb?theme=0" />
+            <SpotifyIframePlaylist src="https://open.spotify.com/embed/playlist/4LddzZkIk08J3IwqXladlJ?theme=0" />
+            <SpotifyIframePlaylist src="https://open.spotify.com/embed/playlist/1aQK4Hz4Xmz3Y4NEhz9ReT?theme=0" />
+            <SpotifyIframePlaylist src="https://open.spotify.com/embed/playlist/26ItkYptyl56YEIYQDQs7r?theme=0" />
+          </Collapsible.Content>
+        </Collapsible.Root>
+
       </main>
     </Layout>
   )
