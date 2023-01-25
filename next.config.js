4640ff023e303f964a5f281f81c8db8385cb3000
setsun <seanjeluo@gmail.com>
2023-01-24 23:05:11 -0500

    initial code for setting up module federation (commented out)

--- next.config.js
+++ next.config.js
@@ -1,3 +1,5 @@
+const { NextFederationPlugin } = require('@module-federation/nextjs-mf');
+
 module.exports = {
   compiler: {
     removeConsole: {
@@ -6,4 +8,27 @@ module.exports = {
     styledComponents: true,
   },
   swcMinify: true,
+  webpack: (config, options) => {
+    // Docs for implementing module federation:
+    // https://github.com/module-federation/universe/tree/main/packages/nextjs-mf#implementing-the-plugin
+    // const { isServer } = options;
+
+    // config.plugins.push(
+    //   new NextFederationPlugin({
+    //     name: 'host',
+    //     remotes: {
+    //       experiments: `experiments@http://localhost:5173/remoteEntry.js`,
+    //     },
+    //     shared: {
+    //       react: {
+    //         singleton: true,
+    //         version: '0'
+    //       }
+    //     }
+    //   })
+    // );
+
+    // Important: return the modified config
+    return config
+  }
 }
