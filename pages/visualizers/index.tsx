ac89b9ca1a36e96c7eedd14792029d14d0d2d6c5
setsun <seanjeluo@gmail.com>
2023-06-30 17:44:37 -0400

    partially fixing module federation

--- pages/visualizers/index.tsx
+++ pages/visualizers/index.tsx
@@ -1,9 +1,17 @@
+import dynamic from 'next/dynamic';
+
 interface Props { }
+
+const VisualizerOne = dynamic(
+  // @ts-ignore
+  () => import('visualizers/VisualizerOne'),
+  { ssr: false },
+);
 
 const Visualizers: React.FC<Props> = (props) => {
   return (
     <div className="h-screen text-3xl flex justify-center items-center">
-      🚧 🚧 🚧
+      <VisualizerOne />
     </div>
   );
 }
