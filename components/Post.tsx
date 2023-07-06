b6e9749002a1ffef99e4416343044798a88e22c3
setsun <setsun.xyz@proton.me>
2023-07-05 23:03:08 -0400

    some clean up

--- components/Post.tsx
+++ components/Post.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import Router from "next/router";
-import styled from "styled-components";
 import ReactMarkdown from "react-markdown";
 
 export type PostProps = {
@@ -14,31 +13,16 @@ export type PostProps = {
   published: boolean;
 };
 
-const Container = styled.div`
-  border: 1px solid white;
-  color: white;
-  padding: 2rem;
-
-  p:last-child {
-    margin-bottom: 0;
-  }
-`;
-
-const PostTitle = styled.h3`
-  margin: 0;
-  margin-bottom: 0.5rem;
-`;
-
 const Post: React.FC<{ post: PostProps }> = ({ post }) => {
   const authorName = post.author ? post.author.name : "Unknown author";
   return (
-    <Container onClick={() => Router.push("/writing/[id]", `/writing/${post.id}`)}>
-      <PostTitle>{post.title}</PostTitle>
+    <div className="border-b-[1px] text-white p-8 last:mb-0" onClick={() => Router.push("/writing/[id]", `/writing/${post.id}`)}>
+      <h3 className="mb-2">{post.title}</h3>
       <small>By {authorName}</small>
       <ReactMarkdown>
        {post.content}
       </ReactMarkdown>
-    </Container>
+    </div>
   );
 };
 
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,27 +2,15 @@ import '../styles/globals.css';
 
 import { Analytics } from '@vercel/analytics/react';
 import { AppProps } from 'next/app';
-import { ThemeProvider, DefaultTheme } from 'styled-components'
-import GlobalStyle from '../components/GlobalStyle';
 import Layout from '../components/Layout';
-
-const theme: DefaultTheme = {
-  colors: {
-    primary: '#0d1117'
-  }
-};
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+    <Layout>
+      <Component {...pageProps} />
 
       <Analytics />
-    </ThemeProvider>
+    </Layout>
   );
 };
 
