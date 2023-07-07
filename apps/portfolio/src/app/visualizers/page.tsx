74a8ec47728b0fbaa4064463d2d1bdce00ff3733
setsun <seanjeluo@gmail.com>
2023-07-07 13:55:58 -0400

    try to fix errors with pre-rendering

--- apps/portfolio/src/app/visualizers/page.tsx
+++ apps/portfolio/src/app/visualizers/page.tsx
@@ -1,44 +1,9 @@
-"use client";
-
-import { PlayIcon, PauseIcon, ExternalLinkIcon } from "@radix-ui/react-icons";
-
-import {
-  VisualizerOne,
-  VisualizerTwo,
-  VisualizerThree,
-  VisualizerFour,
-  VisualizerFive,
-  VisualizerSix,
-} from "visualizers";
+import VisualizerGallery from "../../components/VisualizerGallery";
 
 interface Props {}
 
 const Visualizers: React.FC<Props> = (props) => {
-  return (
-    <div>
-      <VisualizerThree />
-
-      <div className="absolute left-0 top-0 mr-1 hidden h-full w-full text-xs">
-        <a
-          target="_blank"
-          rel="noopener noreferrer"
-          className="inline-flex items-center"
-          href="https://soundcloud.com/thisneverhappenedlabel/virtual-self-ghost-voices-lane-8-remix"
-        >
-          <b>Virtual Self - Ghost Voices (Lane 8 Remix) ↗</b>
-        </a>
-        <p className="m-0">⸻</p>
-
-        <button className="flex items-center">
-          Play <PlayIcon className="ml-1 inline" />
-        </button>
-
-        <p className="font-antonio absolute right-0 top-0 text-3xl">
-          VISUALIZER _01
-        </p>
-      </div>
-    </div>
-  );
+  return <VisualizerGallery />;
 };
 
 export default Visualizers;
