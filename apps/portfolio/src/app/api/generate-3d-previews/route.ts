1bf0f3519b59ba06287bd266e6159f1c45a3bcb9
setsun <seanjeluo@gmail.com>
2023-07-11 15:26:52 -0400

    add puppeteer for scraping video previews

--- apps/portfolio/src/app/api/generate-3d-previews/route.ts
+++ apps/portfolio/src/app/api/generate-3d-previews/route.ts
@@ -1,16 +1,31 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
-export function GET(request: NextRequest) {
-  return NextResponse.json(
-    {
-      body: request.body,
-      path: request.nextUrl.pathname,
-      query: request.nextUrl.search,
-      cookies: request.cookies.getAll(),
-    },
-    {
-      status: 200,
-    }
-  );
+import generate3DPreviews from "@/browser-automation/generate-3d-previews";
+
+export async function GET(request: NextRequest) {
+  try {
+    await generate3DPreviews();
+
+    return NextResponse.json(
+      {
+        body: request.body,
+        path: request.nextUrl.pathname,
+        query: request.nextUrl.search,
+        cookies: request.cookies.getAll(),
+      },
+      {
+        status: 200,
+      }
+    );
+  } catch (e) {
+    return NextResponse.json(
+      {
+        body: `ERROR: ${(e as Error).message}`
+      },
+      {
+        status: 500,
+      }
+    );
+  }
 }
---
+++ apps/portfolio/src/browser-automation/generate-3d-previews.ts
@@ -0,0 +1,47 @@
+import puppeteer from 'puppeteer';
+import { PuppeteerScreenRecorder } from 'puppeteer-screen-recorder';
+
+// todo: add more pages
+const PAGE_NUMBERS = ['1'] as const;
+
+const waitFor = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+
+async function generate3DPreviews() {
+  const browser = await puppeteer.launch({ headless: true });
+  const page = await browser.newPage();
+  const recorder = new PuppeteerScreenRecorder(page, {
+    followNewTab: true,
+    fps: 60,
+    videoFrame: {
+      width: 1600,
+      height: 900,
+    },
+    videoCrf: 24,
+    videoCodec: 'libx264',
+    videoPreset: 'ultrafast',
+    videoBitrate: 1000,
+    autopad: {
+      color: 'black'
+    },
+    aspectRatio: '16:9',
+  });
+
+  // Set screen size
+  await page.setViewport({ width: 1200, height: 700 });
+
+  for (let pageNumber of PAGE_NUMBERS) {
+    await page.goto(`https://setsun.xyz/visualizers/${pageNumber}`, { waitUntil: 'networkidle0' });
+
+    // Start recording
+    const path = `./public/visualizers/${pageNumber}.mp4`;
+    await recorder.start(path);
+
+    await waitFor(3000);
+
+    await recorder.stop();
+  }
+
+  await browser.close();
+}
+
+export default generate3DPreviews;
