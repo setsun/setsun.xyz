ae6a40b2587e402c8872590a8f806d6e68e62692
setsun <seanjeluo@gmail.com>
2023-07-11 16:00:14 -0400

    try to fix puppeteer issues on serverless

--- apps/portfolio/src/browser-automation/generate-3d-previews.ts
+++ apps/portfolio/src/browser-automation/generate-3d-previews.ts
@@ -1,13 +1,29 @@
-import puppeteer from 'puppeteer';
-import { PuppeteerScreenRecorder } from 'puppeteer-screen-recorder';
+import chrome from "chrome-aws-lambda";
+import puppeteer from "puppeteer";
+import { PuppeteerScreenRecorder } from "puppeteer-screen-recorder";
 
 // todo: add more pages
-const PAGE_NUMBERS = ['1'] as const;
+const PAGE_NUMBERS = ["1"] as const;
 
-const waitFor = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+const waitFor = (ms: number) =>
+  new Promise((resolve) => setTimeout(resolve, ms));
+
+async function getPuppeteerOptions() {
+  // Local development
+  // return {
+  //   headless: false
+  // }
+
+  return {
+    args: chrome.args,
+    executablePath: await chrome.executablePath,
+    headless: chrome.headless,
+  };
+}
 
 async function generate3DPreviews() {
-  const browser = await puppeteer.launch({ headless: true });
+  const options = await getPuppeteerOptions();
+  const browser = await puppeteer.launch(options);
   const page = await browser.newPage();
   const recorder = new PuppeteerScreenRecorder(page, {
     followNewTab: true,
@@ -17,20 +33,22 @@ async function generate3DPreviews() {
       height: 900,
     },
     videoCrf: 24,
-    videoCodec: 'libx264',
-    videoPreset: 'ultrafast',
+    videoCodec: "libx264",
+    videoPreset: "ultrafast",
     videoBitrate: 1000,
     autopad: {
-      color: 'black'
+      color: "black",
     },
-    aspectRatio: '16:9',
+    aspectRatio: "16:9",
   });
 
   // Set screen size
   await page.setViewport({ width: 1200, height: 700 });
 
   for (let pageNumber of PAGE_NUMBERS) {
-    await page.goto(`https://setsun.xyz/visualizers/${pageNumber}`, { waitUntil: 'networkidle0' });
+    await page.goto(`https://setsun.xyz/visualizers/${pageNumber}`, {
+      waitUntil: "networkidle0",
+    });
 
     // Start recording
     const path = `./public/visualizers/${pageNumber}.mp4`;
