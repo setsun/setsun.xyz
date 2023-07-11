6a185e89d2ba3e017c5838e49f37d78b83cd9467
setsun <setsun.xyz@proton.me>
2023-07-11 15:26:52 -0400

    add puppeteer for scraping video previews

--- apps/portfolio/next.config.js
+++ apps/portfolio/next.config.js
@@ -1,22 +1,14 @@
-// const NextFederationPlugin = require('@module-federation/nextjs-mf');
+const webpack = require('webpack');
 
 module.exports = {
   reactStrictMode: true,
-  // todo: fix module federation
-  // webpack(config, { isServer }) {
-  //   if (!isServer) {
-  //     // in the browser, federate the following modules for consumption elsewhere
-  //     config.plugins.push(
-  //       new NextFederationPlugin({
-  //         name: 'setsun_xyz',
-  //         filename: 'static/chunks/remoteEntry.js',
-  //         remotes: {
-  //           visualizers: 'visualizers@http://localhost:3001/_next/static/chunks/remoteEntry.js',
-  //         },
-  //       }),
-  //     );
-  //   }
+  webpack(config, { isServer }) {
+    config.plugins.push(
+      new webpack.DefinePlugin({
+        'process.env.FLUENTFFMPEG_COV': false
+      })
+    );
 
-  //   return config;
-  // }
+    return config;
+  }
 };
