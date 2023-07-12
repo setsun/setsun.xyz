5f941f1722ca72339c686cb1dcdba2a4c0a15249
setsun <seanjeluo@gmail.com>
2023-07-12 08:55:37 -0400

    clean up layout

--- apps/portfolio/src/components/Layout.tsx
+++ apps/portfolio/src/components/Layout.tsx
@@ -1,4 +1,3 @@
-import Head from "next/head";
 import React, { ReactNode } from "react";
 import Footer from "@/components/Footer";
 import Navigation from "@/components/Navigation";
@@ -9,14 +8,6 @@ type Props = {
 
 const Layout: React.FC<Props> = (props) => (
   <>
-    <Head>
-      <title>setsun.xyz</title>
-      <link
-        rel="icon"
-        href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🌅</text></svg>"
-      />
-    </Head>
-
     <div className="flex h-screen overflow-hidden">
       <Navigation />
 
