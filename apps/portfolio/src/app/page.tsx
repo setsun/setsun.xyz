b9bedb36d63ea126c9af8df84490680be9deb2cb
setsun <seanjeluo@gmail.com>
2023-07-12 00:58:10 -0400

    customize tailwind theme, and play around with home page

--- apps/portfolio/src/app/page.tsx
+++ apps/portfolio/src/app/page.tsx
@@ -1,16 +1,24 @@
 "use client";
 
 import { Canvas } from "@react-three/fiber";
-import { Metadata } from "next";
-import WireframePlanet from "../components/hero-scenes/WireframePlanet";
 
-// export const metadata: Metadata = {
-//   title: "Docs - Turborepo Example",
-// };
+import WireframePlanet from "@/components/hero-scenes/WireframePlanet";
 
 export default function Home() {
   return (
-    <div className="relative h-screen">
+    <div
+      className="relative h-screen animate-fade-in opacity-0"
+      style={{ animationDelay: "0.2s", animationFillMode: "forwards" }}
+    >
+      <div className="absolute left-0 top-0 flex h-screen w-full items-center justify-center">
+        <h2
+          className="font-antonio animate-pulse opacity-80"
+          style={{ animationDuration: "12s" }}
+        >
+          〰 a perpetual work in progress 〰
+        </h2>
+      </div>
+
       <Canvas
         className="w-full"
         camera={{
