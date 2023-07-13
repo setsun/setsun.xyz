aa9957edbb892d71d8a2a9358db4a81229d431a5
setsun <setsun.xyz@proton.me>
2023-07-13 10:21:57 -0400

    add more basic components

--- packages/ui/src/index.tsx
+++ packages/ui/src/index.tsx
@@ -2,5 +2,10 @@
 import "./styles/globals.css";
 
 // components
-export { Button } from "./components/ui/Button";
-export { Skeleton } from "./components/ui/Skeleton";
+export * from "./components/ui/button";
+export * from "./components/ui/card";
+export * from "./components/ui/context-menu";
+export * from "./components/ui/dialog";
+export * from "./components/ui/input";
+export * from "./components/ui/select";
+export * from "./components/ui/skeleton";
