761a21cd11153d8633862d39817efe1389ff5287
setsun <seanjeluo@gmail.com>
2023-07-14 13:21:02 -0400

    fix imports mb

--- packages/ui/src/index.tsx
+++ packages/ui/src/index.tsx
@@ -2,10 +2,10 @@
 import "./styles/globals.css";
 
 // components
-export * from "./components/ui/button-";
+export * from "./components/ui/button";
 export * from "./components/ui/card";
 export * from "./components/ui/context-menu";
 export * from "./components/ui/dialog";
 export * from "./components/ui/input";
 export * from "./components/ui/select";
-export * from "./components/ui/skeleton-";
+export * from "./components/ui/skeleton";
