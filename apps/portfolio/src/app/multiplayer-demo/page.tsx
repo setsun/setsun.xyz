fad29431b28d0ec958bd68063f3e1391f1d28a0d
setsun <seanjeluo@gmail.com>
2023-07-19 18:12:49 -0400

    fix some stuffff with yjs

--- apps/portfolio/src/app/multiplayer-demo/page.tsx
+++ apps/portfolio/src/app/multiplayer-demo/page.tsx
@@ -3,11 +3,19 @@
 import { OrbitControls } from "@react-three/drei";
 import { Canvas, useFrame } from "@react-three/fiber";
 import { useContext } from "react";
+import * as Y from "yjs";
+import { WebrtcProvider } from "y-webrtc";
 
 import {
   MultiplayerContextProvider,
   MultiplayerContext,
 } from "@/context/MultiplayerContext";
+
+// the shared document / network providers are singletons to be initialized only once
+const yDoc = new Y.Doc();
+const yNetworkProvider = new WebrtcProvider("multiplayer-demo-room", yDoc, {
+  signaling: ['wss://y-webrtc-signaling-eu.herokuapp.com']
+});
 
 const MainScene = () => {
   const { state, onBroadcastLocalPlayerState, getPlayerStates } =
@@ -24,12 +32,15 @@ const MainScene = () => {
     // update all positions of all players
     for (const { id, meshRef } of state.players) {
       const playerState = playerStates.get(id);
-      const playerPosition = playerState.user.position;
+      const playerPosition = playerState?.user?.position;
       const playerMesh = meshRef?.current;
 
+      if (!playerPosition || !playerMesh) continue;
+
       playerMesh?.position?.set?.(
-        // @ts-ignore
-        ...playerPosition
+        playerPosition[0],
+        playerPosition[1],
+        playerPosition[2]
       );
     }
   });
@@ -48,10 +59,7 @@ const MainScene = () => {
 
 export default function Home() {
   return (
-    <MultiplayerContextProvider
-      roomName="multiplayer-demo-room"
-      signalingServerUrl="ws://localhost:4444"
-    >
+    <MultiplayerContextProvider yDoc={yDoc} yNetworkProvider={yNetworkProvider}>
       <div className="relative h-screen text-black">
         <Canvas
           className="w-full"
