402d46c1c286176ba3cb59c285567d648207c6a4
setsun <seanjeluo@gmail.com>
2023-07-26 00:18:29 -0400

    fbo particle simulation

--- apps/portfolio/src/visualizers/14/index.tsx
+++ apps/portfolio/src/visualizers/14/index.tsx
@@ -1,22 +1,68 @@
+import { useFrame } from "@react-three/fiber";
+import { useRef } from "react";
+import { createNoise3D } from "simplex-noise";
+import { CatmullRomCurve3, MeshBasicMaterial, Vector3 } from "three";
+import CustomShaderMaterial from "three-custom-shader-material";
 import { useShaderUniforms } from "veda-ui";
 
 import VisualizerCanvas from "@/components/VisualizerCanvas";
 
-import gradientFragmentShader from "./gradient.frag";
-import gradientVertexShader from "./gradient.vert";
+import displaceTubeVertexShader from "./displace-tube.vert";
+
+const POINTS = [
+  [68.5, 185.5],
+  [1, 262.5],
+  [270.9, 281.9],
+  [300, 212.8],
+  [178, 155.7],
+  [240.3, 72.3],
+  [153.4, 0.6],
+  [52.6, 53.3],
+  [68.5, 185.5],
+];
+
+const noise3d = createNoise3D();
+
+const PATH = new CatmullRomCurve3(
+  POINTS.map((point) => {
+    const x = point[0];
+    const y = (noise3d(point[0], point[1], 0) - 0.5) * 200;
+    const z = point[1];
+
+    return new Vector3(x, y, z);
+  }),
+);
 
 const MainScene = () => {
+  const percentageRef = useRef(0);
+
   const { meshRef, uniforms } = useShaderUniforms({});
 
+  useFrame(({ camera }) => {
+    percentageRef.current += 0.0002;
+
+    const p1 = PATH.getPointAt(percentageRef.current % 1);
+    const p2 = PATH.getPointAt((percentageRef.current + 0.01) % 1);
+
+    camera.position.set(p1.x, p1.y, p1.z);
+    camera.lookAt(p2);
+  });
+
   return (
-    <mesh ref={meshRef}>
-      <planeGeometry args={[16, 16, 128, 128]} />
-      <shaderMaterial
-        vertexShader={gradientVertexShader}
-        fragmentShader={gradientFragmentShader}
-        uniforms={uniforms}
-      />
-    </mesh>
+    <>
+      <mesh ref={meshRef}>
+        <tubeGeometry args={[PATH, 300, 20, 30, true]} />
+
+        <CustomShaderMaterial
+          baseMaterial={MeshBasicMaterial}
+          color="white"
+          transparent
+          wireframe
+          opacity={0.5}
+          uniforms={uniforms}
+        />
+      </mesh>
+    </>
   );
 };
 
@@ -25,9 +71,10 @@ const Visualizer: React.FC<{ fallback?: React.ReactNode }> = ({ fallback }) => {
     <VisualizerCanvas
       headline="VISUALIZER_14"
       audioProps={{
-        name: "Madeon - Miracle",
-        url: "/audio/Miracle.mp3",
-        externalHref: "https://soundcloud.com/madeon/miracle",
+        url: "/audio/Afterglow.mp3",
+        name: "Grum feat. Natalie Shay - Afterglow",
+        externalHref:
+          "https://soundcloud.com/anjunabeats/grum-feat-natalie-shay-afterglow",
       }}
       fallback={fallback}
     >
