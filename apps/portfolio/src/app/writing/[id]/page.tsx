1d856b4e77f13d722fc7c041942560eddd326e26
setsun <setsun.xyz@proton.me>
2023-08-07 17:37:04 -0400

    start to properly use graphql on the client

--- apps/portfolio/src/app/writing/[id]/page.tsx
+++ apps/portfolio/src/app/writing/[id]/page.tsx
@@ -1,38 +1,47 @@
-import { client } from "database";
+"use client";
+
+import { gql, useQuery } from "@urql/next";
 import React from "react";
 import ReactMarkdown from "react-markdown";
 
-async function getPost(postId: string) {
-  const post = await client.post.findUnique({
-    where: {
-      id: postId,
-    },
-    include: {
-      author: {
-        select: { name: true },
-      },
-    },
+const GetPostQuery = gql`
+  query ($id: String) {
+    getPostById(id: $id) {
+      id
+      title
+      content
+      author {
+        name
+      }
+    }
+  }
+`;
+
+const Post = ({ id }: { id: string }) => {
+  const [result] = useQuery({
+    query: GetPostQuery,
+    variables: { id },
   });
 
-  return post;
-}
-
-export default async function Post(props) {
-  const post = await getPost(props.params.id);
-
-  let title = post.title;
-
-  if (!props.published) {
-    title = `${title} (Draft)`;
+  if (!result.data) {
+    return null;
   }
 
+  const post = result.data.getPostById;
+
+  return (
+    <div className="p-4">
+      <h2>{post.title}</h2>
+      <p>By {post.author.name || "Unknown author"}</p>
+      <ReactMarkdown>{post.content}</ReactMarkdown>
+    </div>
+  );
+};
+
+export default function Writing(props) {
   return (
     <>
-      <div className="p-4">
-        <h2>{title}</h2>
-        <p>By {post.author.name || "Unknown author"}</p>
-        <ReactMarkdown>{post.content}</ReactMarkdown>
-      </div>
+      <Post id={props.params.id} />
     </>
   );
 }
