cd80fed137ea9cfdf7424c8e56225b3bee7f20a9
setsun <seanjeluo@gmail.com>
2023-09-20 09:36:42 -0400

    a lil more progress on vis15

--- apps/portfolio/src/projects/visualizers/15/index.tsx
+++ apps/portfolio/src/projects/visualizers/15/index.tsx
@@ -1,21 +1,42 @@
+import { Sky, Stage } from "@react-three/drei";
 import { AudioAnalyser, Color, Vector2 } from "three";
 import { useShaderUniforms } from "veda-ui";
 
 import { Spectogram } from "@/components/Spectogram";
 import VisualizerCanvas, { Pagination } from "@/components/VisualizerCanvas";
 
+import { TheThreeGraces } from "./TheThreeGraces";
+
 const frequencySamples = 256;
 const timeSamples = 512;
 
 const MainScene = ({ analyzer }: { analyzer: AudioAnalyser }) => {
   return (
-    <group position={[0, 0, -40]} rotation={[-Math.PI / 3, 0, -Math.PI / 2]}>
-      <Spectogram
-        audioAnalyser={analyzer}
-        frequencySamples={frequencySamples}
-        timeSamples={timeSamples}
+    <>
+      <Stage />
+
+      <Sky
+        distance={1000}
+        inclination={0.6}
+        azimuth={0.1}
+        mieCoefficient={0.005}
+        mieDirectionalG={0.8}
+        rayleigh={0.5}
+        turbidity={10}
       />
-    </group>
+
+      <group position={[0, -20, -10]} scale={0.1}>
+        <TheThreeGraces />
+      </group>
+
+      <group position={[0, 0, -50]} rotation={[-Math.PI / 3, 0, -Math.PI / 2]}>
+        <Spectogram
+          audioAnalyser={analyzer}
+          frequencySamples={frequencySamples}
+          timeSamples={timeSamples}
+        />
+      </group>
+    </>
   );
 };
 
