febad04ffc0405965ae2f7e44c9979c9b7e2a736
setsun <setsun.xyz@proton.me>
2024-01-13 22:42:29 -0500

    in-progress module federation host

--- apps/portfolio/next.config.mjs
+++ apps/portfolio/next.config.mjs
@@ -1,15 +1,49 @@
+import { NextFederationPlugin } from '@module-federation/nextjs-mf';
+import path from 'path';
+
 /**
  * @type {import('next').NextConfig}
  */
 const config = {
   reactStrictMode: true,
-  webpack: (config) => {
+  webpack: (config, context) => {
+    const { isServer } = context;
+
     // shader support
     config.module.rules.push({
       test: /\.(glsl|vs|fs|vert|frag)$/,
       exclude: /node_modules/,
       use: ['raw-loader', 'glslify-loader'],
     });
+
+    // module federation
+    if (isServer) {
+      config.plugins.push(
+        new NextFederationPlugin({
+          name: 'host',
+          filename: 'static/chunks/remoteEntry.js',
+          exposes: {},
+          remotes: {
+            remote: 'sketchbook-mfe@https://sketchbook.setsun.xyz/remoteEntry.json',
+          },
+          extraOptions: {
+            exposePages: true
+          },
+          shared: {
+            'react': {
+              singleton: true,
+              requiredVersion: false,
+              version: '0',
+            },
+            'react-dom': {
+              singleton: true,
+              requiredVersion: false,
+              version: '0',
+            }
+          },
+        }),
+      );
+    }
 
     return config;
   }
