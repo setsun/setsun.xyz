cd1e334c772d2722d149de34a6f22db5dba179d4
setsun <setsun@raycaster.io>
2026-02-08 02:09:21 -0500

    Configure observability

--- src/router.tsx
+++ src/router.tsx
@@ -1,20 +1,20 @@
-import { createRouter as createTanstackRouter } from '@tanstack/react-router'
+import { createRouter } from "@tanstack/react-router";
 
 // Import the generated route tree
-import { routeTree } from './routeTree.gen'
+import { routeTree } from "./routeTree.gen";
 
 // Create a new router instance
-export const createRouter = () => {
-  return createTanstackRouter({
+export const getRouter = () => {
+  return createRouter({
     routeTree,
     scrollRestoration: true,
     defaultPreloadStaleTime: 0,
-  })
-}
+  });
+};
 
 // Register the router instance for type safety
-declare module '@tanstack/react-router' {
+declare module "@tanstack/react-router" {
   interface Register {
-    router: ReturnType<typeof createRouter>
+    router: ReturnType<typeof getRouter>;
   }
 }
